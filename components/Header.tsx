1be9b4239790d2679125746f4245dcf82330f4f8
lucasrodriguescunha <123827882+lucasrodriguescunha@users.noreply.github.com>
2025-08-09 18:32:23 -0300

    feat: implement Header component with logo and search input

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,6 +1,24 @@
+import Image from 'next/image';
+
+import styles from './Header.module.css';
+
 export default function Header() {
     return (
-        <>
-        </>
+        <div className={styles.headerContainer}>
+            <Image
+                src='/codelab.svg'
+                alt="Logo CodeLab"
+                width={170}
+                height={51}
+            />
+
+            <section className={styles.searchContainer}>
+                <input
+                    type='text'
+                    placeholder='Pesquisar no blog'
+                    className={styles.searchInput}
+                />
+            </section>
+        </div>
     );
 }
